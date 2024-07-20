8d040123057e84b25668876a7b0c263d010fd2df
Mikey Appuhn <mike.appuhn@gmail.com>
2024-07-20 10:39:12 -0700

    made some cleanup changes to the overall file structure

--- midi.py
+++ midi.py
@@ -1,40 +1,15 @@
 import mido
-from midi_play import lpd8_vote
+#from midi_play import lpd8_vote
 import pandas as pd
+
 
 # data pulled from https://github.com/pencilresearch/midi/blob/main/Elektron/Rytm%20MKII.csv 
 
 #port = mido.open_output('Port Name')
 port = mido.open_output(name='foo', virtual=True)
 #print(port)
+rdf = pd.read_csv("Rytm MKII.csv")
 
-def get_cc_lookup(section):
-    rdf = pd.read_csv("Rytm MKII.csv")
-    cc_vals = []
-    for i, j in rdf[rdf['section']==section].iterrows():
-        cc_vals.append(
-            {
-                'section': section,
-                'param': j['parameter_name'],
-                'cc_msb': j['cc_msb'],
-                'cc_min': j['cc_min_value'],
-                'cc_min': j['cc_max_value']
-            }
-        )
-    print(cc_vals)
-
-
-def send_cc(channel=9, cc=1, value=122, time=0):
-    #msg = mido.Message('note_on', note=60)
-    msg = mido.Message(type='control_change', channel=channel, control=cc, value=value, time=time)
-    #mido.Message('control_change', note=60)
-    print(msg)
-    #port.send(msg)
-    
-
-for i in range(0,3):
-    vote = lpd8_vote()
-    send_cc(3,4,vote,0)
 
 
 sections = [
@@ -46,7 +21,6 @@ sections = [
 "LFO"
 ]
 
-get_cc_lookup(sections[0])
 
 synths = [
 "Synth, BD plastic",
@@ -78,7 +52,41 @@ synths = [
 "Synth, impulse"
 ]
 
-print(synths[1])
-get_cc_lookup(synths[1])
+def get_cc_lookup(section):
+    cc_vals = []
+    section_selection = rdf[rdf['section']==section]
+    for i, j in section_selection.iterrows():
+        cc_vals.append(
+            {
+                'section': section,
+                'param': j['parameter_name'],
+                'cc_msb': j['cc_msb'],
+                'cc_min': j['cc_min_value'],
+                'cc_min': j['cc_max_value']
+            }
+        )
+    print(cc_vals)
 
 
+def send_cc(channel=9, cc=1, value=122, time=0):
+    #msg = mido.Message('note_on', note=60)
+    msg = mido.Message(type='control_change', channel=channel, control=cc, value=value, time=time)
+    #mido.Message('control_change', note=60)
+    print(msg)
+    #port.send(msg)
+    
+def main():
+    """
+    for i in range(0,3):
+        vote = lpd8_vote()
+        #send_cc(3,4,vote,0)
+        print(vote)
+    """
+
+    for i in range(0, len(sections)):
+        get_cc_lookup(sections[i])
+    print(synths[1])
+    get_cc_lookup(synths[1])
+
+
+main()
